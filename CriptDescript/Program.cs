63c72993c01f111e2c7d8d5821a0c43d432caca7
anderson.bellini <anderson.bellini@anac.gov.br>
2018-10-31 11:05:58 -0300

    Add project files.

---
+++ CriptDescript/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CriptDecript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "AndersonBellini"; // Data to save
+            var password = "CriptDecript"; //Key To Use for recover data
+            var strEncryptred = Bellini.Encrypt(str, password);
+            var strDecrypted = Bellini.Decrypt(strEncryptred, password);
+        }
+
+    }
+}
